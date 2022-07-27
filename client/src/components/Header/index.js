f0d2084b55805d09cc6dd76470d2a4478f5c0264
BrandonDaws <velarde.brandon02@gmail.com>
2022-07-26 20:18:05 -0500

    working on tailwindcss setup, pushing so we can all have updated code

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,21 @@
+
+<nav class="">
+<p>Icon goes here </p>
+<section>
+        <h1>
+            <a href="/" role="button">Title</a>
+        </h1>
+        
+        
+        <a class="" href="/">Dashboard</a>
+
+        <a role="button" class="" href="/">Home</a>
+       
+        <a role="button" class="" href="/" id="">holder</a>
+        
+        <a role="button" class="" href="/">holder</a>
+        
+      
+</section>
+
+    </nav>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'custom-teal': '#2b6777',
+        'custon-blue': '#c8d8e4',
+        'custon-white': '#ffffff',
+        'custon-grey': '#f2f2f2',
+        'custom-aqua': '#52ab98'
+      }
+    },
+    fontFamily:{
+      
+    }
   },
   plugins: [],
 }
