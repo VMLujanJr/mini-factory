6d3e8fd90ca4a339775b58b6cc538d3d89fc33ec
Michael Tulmen <michaeltulmen@gmail.com>
2022-07-26 20:20:52 -0500

    server.js cleanup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 // this imports the Apollo Server
 const { ApolloServer } = require("apollo-server-express");
+const path = require('path');
 
 // imports typeDefs, resolvers
 const { typeDefs, resolvers } = require("./schemas");
 //DB locale
+const { authMiddleware } = require('./utils/auth');
 const db = require("./config/connection");
 
 //This is server location FOR NOW, will see with further deployment
@@ -13,6 +15,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
     typeDefs,
     resolvers
+    context: authMiddleware,
 });
 
 const app = express();
---
+++ server/utils/dateFormat.js
@@ -0,0 +1,1 @@
+const addDateSuffix = (date) => 
