fd5fb7ebf9d108dc065dd2b774189ea6895c23bd
BrandonDaws <velarde.brandon02@gmail.com>
2022-07-27 20:18:16 -0500

    added tailwind plugin and finished config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,33 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+const { screens } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors:{
+      white: colors.white,
+      black: colors.black,
+      gray: colors.gray,
+      sky: colors.sky,
+      cyan: colors.cyan,
+      emerald: colors.emerald
+    },
     extend: {
       colors:{
         'custom-teal': '#2b6777',
         'custon-blue': '#c8d8e4',
         'custon-white': '#ffffff',
-        'custon-grey': '#f2f2f2',
+        'custon-gray': '#f2f2f2',
         'custom-aqua': '#52ab98'
       }
     },
+    
     fontFamily:{
       gothic: 'century-gothic, sans-serif',
       default: 'montserrat, sans-serif'
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio')
+  ],
 }
