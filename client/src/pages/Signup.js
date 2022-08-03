63d3aacc9208b5d0f7d1fbcaf94a6f7d6f2ca1b3
VMLujanJr <vmlujanjr@outlook.com>
2022-08-03 15:24:22 -0500

    new changes

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,14 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth.js';
 
 const Signup = () => {
+    const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+    const [addUser, { error }] = useMutation(ADD_USER);
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const { data } = await addUser({
+                variables: { ...formState }
+            });
+            
+            Auth.login(data.addUser.token);
+        }
+        catch (e) {
+            console.error(e);
+        }
+    };
+
     return(
-        <section className="flex w-[30rem] flex-col space-y-10 m-3 justify-self-center">
+        <section className="flex w-[30rem] flex-col space-y-10 m-3">
             <div className="text-center text-4xl font-medium">Signup</div>
             <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
-                <input type="text" placeholder="Email" className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none" />
-            </div>
-            <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
-                <input type="password" placeholder="Password" className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none" />
+                <form onSubmit={handleFormSubmit}>
+                    <input
+                        className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+                        placeholder="Username" 
+                        name='username'
+                        type='username'
+                        id='username'
+                        value={ formState.username }
+                        onChange={ handleChange }
+                    />
+                    <input
+                        className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+                        placeholder="Email" 
+                        name="email"
+                        type="email"
+                        id="email"
+                        value={ formState.email }
+                        onChange={ handleChange }
+                    />
+                    <input
+                        className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+                        placeholder="******" 
+                        name="password"
+                        type="password"
+                        id="password"
+                        value={ formState.password }
+                        onChange={ handleChange }
+                    />
+                </form>
             </div>
             <button className="transform rounded-sm bg-indigo-600 py-2 font-bold duration-300 hover:bg-blue-400">SIGN UP</button>
         </section>
