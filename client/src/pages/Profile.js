41db95ac1ce6d8293c593f6ce29bc33cd6a33aee
BrandonDaws <velarde.brandon02@gmail.com>
2022-08-03 20:18:31 -0500

    mvp for profile css

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,46 +3,46 @@ import React from "react";
 const Profile = () => {
     return (
         <>
-            <section className="bg-white py-8">
+            <section className="bg-custom-aqua py-8">
                 <div className="container py-8 px-6 mx-auto">
-                    <h3 className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl mb-8">
+                    <h3 className="text-custom-white uppercase tracking-wide no-underline hover:no-underline font-bold  text-xl mb-8">
                         (User's Name Goes Here)'s Page!'
                     </h3>
-                    <p className="mt-8 mb-8">
+                    <p className="text-custom-white mt-8 mb-8">
                         Welcome to My Page! Checkout my Mini's and let me know if anything peaks
                         your interest!{" "}
                     </p>
                     <br />
-                    <p className="mt-8 mb-8">
+                    <p className="text-custom-white mt-8 mb-8">
                         {" "}
                         Careful Some Mini's are more aggresiv than others!{" "}
                     </p>
                 </div>
             </section>
             <section
-                className="w-full mx-auto bg-nordic-gray-light flex pt-12 md:pt-0 md:items-center bg-cover bg-right"
+                className="w-full mx-auto bg-custom-blue text-custom-white flex pt-12 md:pt-0 md:items-center bg-cover bg-right"
                 style={{ maxWidth: 1600, height: "32rem" }}
             >
                 <div className="container mx-auto">
                     <div className="flex flex-col w-full lg:w-1/2 justify-center items-start  px-6 tracking-wide">
-                        <h1 className="text-black text-2xl my-4">
-                            Stripy Zig Zag Jigsaw Pillow and Duvet Set
+                        <h1 className="text-custom white text-2xl my-4">
+                            Current Mini's on Deck:
                         </h1>
                         <div
                             className="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black"
                             href=""
                         >
-                            products
+                            Beware!
                         </div>
                     </div>
                 </div>
             </section>
-            <section className="bg-white py-8">
+            <section className="bg-custom-aqua py-8">
                 <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
                     <nav id="store" className="w-full z-30 top-0 px-6 py-1">
                         <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
                             <div
-                                className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl"
+                                className="uppercase tracking-wide no-underline hover:no-underline font-bold text-custom-white text-xl"
                             >
                                 Store
                             </div>
@@ -83,7 +83,7 @@ const Profile = () => {
                                 src="https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80"
                                 alt='my miniature'
                             />
-                            <div className="pt-3 flex items-center justify-between">
+                            <div className="pt-3 flex text-custom-white items-center justify-between">
                                 <p className="">Product Name</p>
                                 <img
                                     className="h-6 w-6 fill-current text-gray-500 hover:text-black"
@@ -91,7 +91,7 @@ const Profile = () => {
                                     alt='my miniature'
                                 />
                             </div>
-                            <p className="pt-1 text-gray-900">£9.99</p>
+                            <p className="pt-1 text-custom-white">£9.99</p>
                         </div>
                     </div>
                     <div className="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
@@ -101,7 +101,7 @@ const Profile = () => {
                                 src="https://images.unsplash.com/photo-1508423134147-addf71308178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80"
                                 alt=""
                             />
-                            <div className="pt-3 flex items-center justify-between">
+                            <div className="pt-3 flex text-custom-white items-center justify-between">
                                 <p className="">Product Name</p>
                                 <img
                                     className="h-6 w-6 fill-current text-gray-500 hover:text-black"
@@ -109,7 +109,7 @@ const Profile = () => {
                                     alt=""
                                 />
                             </div>
-                            <p className="pt-1 text-gray-900">£9.99</p>
+                            <p className="pt-1 text-custom-white">£9.99</p>
                         </div>
                     </div>
                 </div>
